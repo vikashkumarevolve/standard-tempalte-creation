0016482ea5c61448e0cc262e8e27e09fd48552df
vikashkumarevolve <vikashkumarevolve>
2024-01-04 18:46:44 +0530

    added files

--- template.py
+++ template.py
@@ -2,29 +2,32 @@ import os
 from pathlib import Path
 
 list_of_files=[
+
     ".github/workflows/.gitkeep",
     "src/__init__.py",
     "src/components/__init__.py",
     "src/components/data_ingestion.py",
     "src/components/data_transformation.py",
-    "src/components/data_trainer.py",
+    "src/components/model_trainer.py",
     "src/components/model_evaluation.py",
     "src/pipeline/__init__.py",
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
+    "src/utils/__init__.py",
+    "src/utils/utils.py",
     "src/logger/logging.py",
     "src/exception/exception.py",
-    "src/utils/utils.py",
     "tests/unit/__init__.py",
     "tests/integration/__init__.py",
     "init_setup.sh",
     "requirements.txt",
     "requirements_dev.txt",
-    "setup.cfg",
     "setup.py",
+    "setup.cfg",
     "pyproject.toml",
     "tox.ini",
     "experiment/experiments.ipynb"
+
 ]
 
 for filepath in list_of_files:
